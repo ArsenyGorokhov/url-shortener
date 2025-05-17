815cfc2d5f3bf8767a0099e653d8eeded33f7d51
Arseny Gorokhov <Arseny.Gorokhoff@yandex.ru>
2025-05-17 17:30:42 +0500

    os.Getenv -> configPath := "..."

--- internal/config/config.go
+++ internal/config/config.go
@@ -21,10 +21,13 @@ type HTTPServer struct {
 }
 
 func MustLoad() *Config {
-	configPath := os.Getenv("CONFIG_PATH")
-	if configPath == "" {
-		log.Fatal("CONFIG_PATH is not set")
-	}
+	// ошибка, не читается энв
+	// configPath := os.Getenv("CONFIG_PATH")
+	// if configPath == "" {
+	// 	log.Fatal("CONFIG_PATH is not set")
+	// }
+
+	configPath := "../../config/local.yaml"
 
 	if _, err := os.Stat(configPath); err != nil {
 		log.Fatalf("config file does not exist: %v", err)
